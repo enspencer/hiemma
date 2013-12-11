7092341bdfa352361d532f5a52c2b989fbc88d5f
Emma Spencer <emma.n.spencer@gmail.com>
2013-12-11 10:04:06 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hiemma::Application.config.secret_key_base = '762512b385ea0b579677effae508e1d147cd997fbcf19e54e5dc8692de46858360ac14ad6f30eb526dac7be5e6cfef31771d551f22b3be488c677887409c3f90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hiemma::Application.config.session_store :cookie_store, key: '_hiemma_session'
